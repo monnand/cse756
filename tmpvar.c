edbe61450c440425156434564fdec1ac75ddb10e
Monnand <monnand@gmail.com>
2011-05-11 02:51:44 -0400

    tmpvar test case

---
+++ tmpvar.c
@@ -0,0 +1,22 @@
+int main() {
+    int aarr[10];
+    int a = 1;
+
+    int b;
+    b = a + 1;
+    aarr[b + 3] = 10;
+
+    for (a = 0; a < 100; a++)
+        b = a + 1;
+
+    if (b > 100)
+        a = b + a + 100;
+    else
+        a = aarr[3];
+
+    a = 0;
+    while (a < 100) {
+        b = a + 1;
+        a = b;
+    }
+}
