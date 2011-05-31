0784493229ba1fd757215c67e219af8a491253bb
Monnand <monnand@gmail.com>
2011-05-30 23:56:14 -0400

    basic blocks
    todo: CFG. i.e. build edges between basic blocks

--- cfg.cpp
+++ cfg.cpp
@@ -18,6 +18,7 @@ enum InstructionType {
     COND_JMP_INST,
     UNCOND_JMP_INST,
     ASSIGN_OP_INST,
+    RETURN_INST,
     NR_INST_TYPE
 };
 
@@ -27,11 +28,17 @@ class Instruction {
         int type;
         int id;
         list<string> labs;
+        string str_labs;
     public:
         Instruction(SgStatement *stmt, int id) {
             this->stmt = stmt;
             this->id = id;
         }
+
+        bool is_jmp() { return type == COND_JMP_INST || type == UNCOND_JMP_INST; }
+        bool is_cond_jmp() { return type == COND_JMP_INST; }
+        bool is_uncond_jmp() { return type == UNCOND_JMP_INST; }
+        bool is_valid() { return type >= COND_JMP_INST && type < NR_INST_TYPE; }
 
         int get_id() { return id; }
         void set_id(int id) { this->id = id; }
@@ -39,15 +46,37 @@ class Instruction {
         virtual ~Instruction() {
         }
 
-        virtual const char *get_dst() = 0;
+        virtual const char *get_dst() { return NULL; }
 
+        const list<string> &get_labs() { return labs; }
         int add_labs(list<string> &l);
+        bool in_labs(string lab);
+        bool in_labs(list<string> &l);
+
+        const char *get_original_code() { return (str_labs + stmt->unparseToString()).c_str(); }
 };
+
+bool Instruction::in_labs(string lab) {
+    list<string>::iterator iter;
+    for (iter = labs.begin(); iter != labs.end(); iter++)
+        if (lab == *iter)
+            return true;
+    return false;
+}
+
+bool Instruction::in_labs(list<string> &l) {
+    list<string>::iterator iter;
+    for (iter = l.begin(); iter != l.end(); iter++)
+        if (in_labs(*iter))
+            return true;
+    return false;
+}
 
 int Instruction::add_labs(list<string> &l) {
     list<string>::iterator iter;
     for (iter = l.begin(); iter != l.end(); iter++) {
         labs.push_back(*iter);
+        str_labs += (*iter) + ": ";
     }
     return labs.size();
 }
@@ -56,8 +85,6 @@ class ThreeAddressInstruction : public Instruction {
     public:
         ThreeAddressInstruction(SgStatement *stmt, int id = 0);
         virtual ~ThreeAddressInstruction();
-
-        virtual const char *get_dst() { return NULL; }
     private:
         string oprand1, oprand2;
         string dst;
@@ -72,6 +99,12 @@ ThreeAddressInstruction::ThreeAddressInstruction(SgStatement *stmt, int id)
 ThreeAddressInstruction::~ThreeAddressInstruction() {
 }
 
+class ReturnInstruction : public Instruction {
+    public:
+        ReturnInstruction(SgStatement *stmt, int id = 0) : Instruction(stmt, id) { type = RETURN_INST; }
+        virtual ~ReturnInstruction() {}
+};
+
 class UncondJmp : public Instruction {
     public:
         UncondJmp(SgStatement *stmt, int id = 0);
@@ -83,13 +116,13 @@ class UncondJmp : public Instruction {
 
 UncondJmp::UncondJmp(SgStatement *stmt, int id)
     : Instruction(stmt, id) {
-
     SgGotoStatement *goto_stmt = isSgGotoStatement(stmt);
     if (NULL == goto_stmt)
         /* TODO Exception */
         return;
     SgLabelStatement *lab_stmt = goto_stmt->get_label();
     dstlab = lab_stmt->get_label().getString();
+    type = UNCOND_JMP_INST;
 }
 
 class CondJmp : public Instruction {
@@ -104,8 +137,11 @@ class CondJmp : public Instruction {
 
 CondJmp::CondJmp(SgStatement *stmt, int id)
     : Instruction(stmt, id) {
+
     SgIfStmt *ifstmt = isSgIfStmt(stmt);
     SgExprStatement *expr_stmt;
+
+    type = COND_JMP_INST;
 
     cond = NULL;
     if (NULL == ifstmt)
@@ -123,17 +159,81 @@ CondJmp::CondJmp(SgStatement *stmt, int id)
     dstlab = uncond.get_dst();
 }
 
+class BasicBlock {
+    private:
+        list<Instruction *> inst_list;
+        static int nr_blk;
+        int id;
+
+        list<string> dst_labs;
+        list<string> labs;
+    public:
+        BasicBlock() {
+            id = nr_blk;
+            nr_blk++;
+        }
+        virtual ~BasicBlock() {}
+        int add_instruction(Instruction *inst);
+        void basic_info(ostream &out) {
+            out << "block id: " << id << endl;
+            out << "\tPoint to ";
+            list<string>::iterator iter;
+            for (iter = dst_labs.begin(); iter != dst_labs.end(); iter++) {
+                out << *iter << " ";
+            }
+            out << endl << "\tLabs: ";
+            for (iter = labs.begin(); iter != labs.end(); iter++) {
+                out << *iter << " ";
+            }
+
+            out << endl << "\tInstruction:" << endl;
+
+            list<Instruction *>::iterator iiter;
+            for (iiter = inst_list.begin(); iiter != inst_list.end(); iiter++) {
+                Instruction *i = *iiter;
+                out << "\t  " << i->get_original_code() << endl;
+            }
+            out << endl;
+        }
+};
+
+int BasicBlock::nr_blk = 0;
+
+int BasicBlock::add_instruction(Instruction *inst) {
+    const list<string> &ilabs = inst->get_labs();
+    list<string>::const_iterator iter;
+
+    for(iter = ilabs.begin(); iter != ilabs.end(); iter++) {
+        string l = *iter;
+        labs.push_back(l);
+    }
+
+    if (inst->is_jmp()) {
+        string dst = inst->get_dst();
+        dst_labs.push_back(dst);
+    }
+
+    inst_list.push_back(inst);
+
+    return 0;
+}
+
 class InstructionList {
     private:
         list<Instruction *> ilist;
         list<int> leaders;
         list<string> labs;
+        list<string> jmp_dsts;
+        list<int> jmp_next;
+
+        bool is_jmp_next;
 
     protected:
         Instruction *append(SgExprStatement *expr);
+        Instruction *append(SgVariableDeclaration *vardecl);
 
     public:
-        InstructionList() {}
+        InstructionList() { is_jmp_next = true; }
         virtual ~InstructionList() {
             list<Instruction *>::iterator iter;
             for (iter = ilist.begin(); iter != ilist.end(); iter++) {
@@ -142,32 +242,75 @@ class InstructionList {
             }
         }
 
+        list<Instruction *> &get_inst_list() { return ilist; }
+
         Instruction *append(SgStatement *stmt);
-        Instruction *append(SgVariableDeclaration *vardecl);
+        bool is_leader(Instruction *inst);
+        int build_leaders();
+        int build_basic_blocks(list<BasicBlock *> &blist);
 };
+
+int InstructionList::build_basic_blocks(list<BasicBlock *> &blist) {
+    BasicBlock *block = NULL;
+    list<Instruction *>::iterator iter;
+    for (iter = ilist.begin(); iter != ilist.end(); iter++) {
+        Instruction *inst = *iter;
+        if (is_leader(inst)) {
+            if (NULL != block)
+                blist.push_back(block);
+            block = new BasicBlock();
+        }
+        if (NULL != block)
+            block->add_instruction(inst);
+    }
+    if (NULL != block)
+        blist.push_back(block);
+
+    return 0;
+}
+
+bool InstructionList::is_leader(Instruction *inst) {
+    if (NULL == inst)
+        return false;
+
+    list<string>::iterator iter;
+    for(iter = jmp_dsts.begin(); iter != jmp_dsts.end(); iter++) {
+        string dst = *iter;
+
+        if (inst->in_labs(dst)) {
+            return true;
+        }
+    }
+
+    list<int>::iterator int_iter;
+    for(int_iter = jmp_next.begin(); int_iter != jmp_next.end(); int_iter++) {
+        if (*int_iter == inst->get_id()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int InstructionList::build_leaders() {
+    leaders.clear();
+    list<Instruction *>::iterator iter;
+    for (iter = ilist.begin(); iter != ilist.end(); iter++) {
+        Instruction *inst = *iter;
+        if (is_leader(*iter)) {
+            leaders.push_back((*iter)->get_id());
+        }
+    }
+    return 0;
+}
 
 Instruction *InstructionList::append(SgVariableDeclaration *vardecl) {
     Instruction *inst = NULL;
-    /*
-    cout << "[VarDecl] " << vardecl->unparseToString() << endl;
-    */
     return inst;
 }
 
 Instruction *InstructionList::append(SgExprStatement *expr) {
     Instruction *inst = NULL;
-
-    /*
-    list<string>::iterator iter;
-
-    for (iter = labs.begin(); iter != labs.end(); ++iter) {
-        cout << *iter << ": ";
-    }
-    cout << "[ExprStmt] " << expr->unparseToString() << endl;
-    */
-
     inst = new ThreeAddressInstruction((SgStatement *)expr);
-
     return inst;
 }
 
@@ -178,23 +321,28 @@ Instruction *InstructionList::append(SgStatement *stmt) {
         {
             SgVariableDeclaration *vardecl = isSgVariableDeclaration(stmt);
             inst = append(vardecl);
+            break;
         }
         case V_SgExprStatement:
         {
             SgExprStatement *expr_stmt = isSgExprStatement(stmt);
             inst = append(expr_stmt);
+            break;
         }
         case V_SgReturnStmt:
         {
-            cout << "[ReturnStmt] " << stmt->unparseToString() << endl;
             labs.clear();
+            inst = new ReturnInstruction(stmt);
             break;
         }
         case V_SgIfStmt: 
         {
             inst = new CondJmp(stmt);
-            cout << "[IfStmt] " << stmt->unparseToString() << endl;
-            cout << "\tjmp dst: " << inst->get_dst() << endl;
+            break;
+        }
+        case V_SgGotoStatement:
+        {
+            inst = new UncondJmp(stmt);
             break;
         }
         case V_SgLabelStatement:
@@ -215,28 +363,22 @@ Instruction *InstructionList::append(SgStatement *stmt) {
         inst->set_id(ilist.size());
         labs.clear();
         ilist.push_back(inst);
-        if (0 == ilist.size())
-            leaders.push_back(inst->get_id());
+        if (is_jmp_next) {
+            jmp_next.push_back(inst->get_id());
+            is_jmp_next = false;
+        }
+        if (inst->is_jmp()) {
+            is_jmp_next = true;
+            string dst = inst->get_dst();
+            jmp_dsts.push_back(dst);
+        } else {
+            is_jmp_next = false;
+        }
     }
     return inst;
 }
 
 /*
-class BasicBlock {
-    private:
-        list<Instruction *> inst_list;
-        static int nr_blk;
-        int id;
-    public:
-        BasicBlock() {
-            id = nr_blk;
-            nr_blk++;
-        }
-        virtual ~BasicBlock() {}
-};
-
-int BasicBlock::nr_blk = 0;
-
 class ControlFlowGraph {
     public:
         ControlFlowGraph() {}
@@ -260,6 +402,16 @@ int examine_scope(SgScopeStatement *scope, InstructionList *ilist) {
         SgStatement *stmt = *stmt_iter;
         ilist->append(stmt);
     }
+
+    list<BasicBlock *> blist;
+    ilist->build_basic_blocks(blist);
+
+    list<BasicBlock *>::iterator iter;
+
+    for(iter = blist.begin(); iter != blist.end(); iter++) {
+        (*iter) -> basic_info(cout);
+    }
+
     return 0;
 }
 
